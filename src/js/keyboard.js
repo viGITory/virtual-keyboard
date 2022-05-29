6322cfee2af3ccd9953c4c61dd18701cb2f8b549
PANTORY <pantory@gmail.com>
2022-05-29 12:55:25 +0300

    feat: add keyboard row with current lang button and shortcut description

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -11,6 +11,35 @@ class Keyboard {
   }
 
   render() {
+    this.createRows();
+
+    document.body.append(display.render(), this.container);
+  }
+
+  createTopRow() {
+    this.activeLang = document.createElement('button');
+    const row = document.createElement('div');
+    const description = document.createElement('p');
+    const emptyButton = document.createElement('button');
+
+    this.activeLang.textContent =
+      localStorage.getItem('vigitory-lang') === JSON.stringify(en)
+        ? 'en'
+        : 'ru';
+    description.textContent = 'Press Ctrl + Alt to switch language';
+
+    this.activeLang.classList.add('keyboard__key', 'keyboard__key--lang');
+    row.classList.add('keyboard__bar');
+    description.classList.add('keyboard__description');
+    emptyButton.classList.add('keyboard__key', 'keyboard__key--empty');
+
+    this.activeLang.setAttribute('type', 'button');
+    emptyButton.setAttribute('type', 'button');
+
+    return [this.activeLang, description, emptyButton];
+  }
+
+  createRows() {
     const buttons = [];
 
     this.keys = this.currentLang.map((item) => {
@@ -20,23 +49,23 @@ class Keyboard {
       return key;
     });
 
-    for (let i = 0; i < 5; i += 1) {
+    for (let i = 0; i < 6; i += 1) {
       const row = document.createElement('div');
       row.classList.add('keyboard__row');
 
-      if (i === 0) row.append(...buttons.slice(0, 14));
-      else if (i === 1) row.append(...buttons.slice(14, 28));
-      else if (i === 2) row.append(...buttons.slice(28, 41));
-      else if (i === 3) row.append(...buttons.slice(41, 54));
+      if (i === 0) row.append(...this.createTopRow());
+      else if (i === 1) row.append(...buttons.slice(0, 14));
+      else if (i === 2) row.append(...buttons.slice(14, 28));
+      else if (i === 3) row.append(...buttons.slice(28, 41));
+      else if (i === 4) row.append(...buttons.slice(41, 54));
       else row.append(...buttons.slice(54));
 
       this.container.append(row);
     }
-
-    document.body.append(display.render(), this.container);
   }
 
   switchCase() {
+    this.activeLang.textContent = this.currentLang === en ? 'en' : 'ru';
     this.currentLang.forEach((data, index) => {
       this.keys[index].switchCase(data);
     });
@@ -61,6 +90,13 @@ class Keyboard {
       if (!event.shiftKey && !event.getModifierState('CapsLock')) {
         this.switchCase();
       }
+    });
+
+    this.activeLang.addEventListener('click', () => {
+      this.currentLang = this.currentLang === en ? ru : en;
+      this.activeLang.textContent = this.currentLang === en ? 'en' : 'ru';
+
+      this.switchCase();
     });
   }
 
